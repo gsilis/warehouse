8793da75c29d352383d8e7ac86f3f572aab23100
Girts Silis <girts.silis@gmail.com>
2025-11-15 14:46:43 -0600

    + tweak for mobile

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -6,7 +6,7 @@ import { FlagToggle } from "./FlagToggle";
 export function Controls() {
   const boxContext = useContext(BoxContext);
 
-  return <div className="bg-slate-950 overflow-scroll flex-[0_200px] md:flex-[0_500px] flex flex-col md:justify-center">
+  return <div className="bg-slate-950 overflow-scroll flex-[0_200px] md:flex-[0_500px] flex flex-col md:justify-center pb-[60px] sm:pb-0">
     <ShelfControl id={ 0 } boxes={ boxContext.boxes }>1</ShelfControl>
     <ShelfControl id={ 1 } boxes={ boxContext.boxes }>2</ShelfControl>
     <ShelfControl id={ 2 } boxes={ boxContext.boxes }>3</ShelfControl>
